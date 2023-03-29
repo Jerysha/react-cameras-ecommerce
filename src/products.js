261e2cbcc0711fefeaadc0ab8c60980401f05e70
Jerysha <jerysha.s.a@gmail.com>
2023-03-28 19:21:46 -0600

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,58 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+
+export const PRODUCTS = [{
+        id: 1,
+        productName: "Sony Tripode",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "Canon EDS 7D",
+        price: 240.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Nikon Pro",
+        price: 499.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Panasonix HC-X1E Zwart",
+        price: 700.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Sony ZEISS RX1",
+        price: 175.99,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Canon EOS 60D",
+        price: 250.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Canon EOS 7D",
+        price: 340.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "RED Epic-W Helium 8K S35",
+        price: 899.0,
+        productImage: product8,
+    },
+];
